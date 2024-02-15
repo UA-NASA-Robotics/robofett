7628320109fac08db4a86dc250a49fcf36360352
nobrien1 <81879104+nobrien1@users.noreply.github.com>
2024-02-15 00:58:46 +0000

    Rename files

--- Controlling_Motors/Controlling_Motors.ino
+++ Controlling_Motors/Controlling_Motors.ino
@@ -5,14 +5,116 @@
 #include <iostream> 
 #include <chrono> 
 
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+// #define NOTE_C5  523
+// #define NOTE_CS5 554
+// #define NOTE_D5  587
+// #define NOTE_DS5 622
+// #define NOTE_E5  659
+// #define NOTE_F5  698
+// #define NOTE_FS5 740
+// #define NOTE_G5  784
+// #define NOTE_GS5 831
+// #define NOTE_A5  880
+// #define NOTE_AS5 932
+// #define NOTE_B5  988
+#define NOTE_C5  1047
+#define NOTE_CS5 1109
+#define NOTE_D5  1175
+#define NOTE_DS5 1245
+#define NOTE_E5  1319
+#define NOTE_F5  1397
+#define NOTE_FS5 1480
+#define NOTE_G5  1568
+#define NOTE_GS5 1661
+#define NOTE_A5  1760
+#define NOTE_AS5 1865
+#define NOTE_B5  1976
+#define NOTE_C6  2093
+#define NOTE_CS6 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
 template <typename T>
 struct Wheels {
   T left;
   T right;
 };
 
-const Wheels<int> WHEEL_PINS = {33, 32};
-//const int BUZZER_PIN = 32;
+struct Note {
+  unsigned int frequency;
+  double duration;
+};
+
+const Wheels<int> WHEEL_PINS = {33, 14};
+const int BUZZER_PIN = 32;
+
+Note billieJeanNotes[] = {{NOTE_FS5, 0.5}, {NOTE_FS5, 0.5}, {NOTE_E5, 0.5}, {NOTE_CS5, 0.5}, {NOTE_CS5, 1}, 
+                          {NOTE_FS5, 0.5}, {NOTE_FS5, 1}, {NOTE_E5, 1}, {NOTE_CS5, 2}, 
+                          {NOTE_FS5, 0.5}, {NOTE_FS5, 0.5}, {NOTE_FS5, 0.5}, {NOTE_E5, 0.5}, {NOTE_CS5, 1.5},  
+                          {NOTE_FS5, 0.5}, {NOTE_A5, 1}, {NOTE_B5, 1}, {NOTE_A5, 0.5}, {NOTE_GS5, 0.5}, {NOTE_FS5, 3}, 
+                          {NOTE_FS5, 1}, {NOTE_FS5, 0.5}, 
+                          {NOTE_CS6, 0.5}, {NOTE_B5, 1.5}, {NOTE_FS5, 0.5}, {NOTE_D5, 1}, {NOTE_CS5, 2}};
 
 GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
 Servo servoRight, servoLeft;
@@ -90,6 +192,7 @@ void setup() {
 
   pinMode(WHEEL_PINS.left, OUTPUT);
   pinMode(WHEEL_PINS.right, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
   if (!servoRight.attached()) {
 		servoRight.setPeriodHertz(50); // standard 50 hz servo
@@ -289,4 +392,82 @@ Wheels<int> getWheelsDutyCycle(GamepadPtr myGamepad) {
   output.left += 128;
 
   return output;
+}
+
+void billieJeanEmote() {
+  if(isEmoting) return;
+  isEmoting = true;
+
+  Serial.println("emoting");
+
+  writeWheels(128, 128);
+
+  delay(1000);
+  
+  for(int i = 0; i < 4; i++) {
+    writeWheels(255, 255);
+    delay(100);
+    writeWheels(128, 128);
+    delay(400);
+
+    writeWheels(0, 0);
+    delay(100);
+    writeWheels(128, 128);
+    delay(400);
+  }
+
+  delay (500);
+
+  for(int i = 0; i < 2; i++) {
+    writeWheels(128, 255);
+    delay(100);
+    writeWheels(128, 128);
+    delay(400);
+  }
+
+  delay(1000);
+
+  writeWheels(0, 255);
+  delay(1000);
+
+  writeWheels(128, 128);
+  delay(1000);
+
+  writeWheels(0, 0);
+  delay(2000);
+
+  writeWheels(0, 255);
+  delay(1000);
+
+  writeWheels(0, 0);
+  delay(2000);
+
+  writeWheels(128, 128);
+  
+  isEmoting = false;
+}
+
+void playSong(Note* notes, int tempo) {
+  if(isPlayingSong) return;
+  isPlayingSong = true;
+
+  Serial.println("song");
+
+  noTone(BUZZER_PIN);
+  for(int i = 0; i < 27; i++) {
+    int duration = 1000 * notes[i].duration / (tempo / 60);
+    if(notes[i].frequency <= 0) {
+      noTone(BUZZER_PIN);
+      delay(notes[i].duration);
+    } else {
+      tone(BUZZER_PIN, notes[i].frequency);
+      delay(duration * 2/3);
+      noTone(BUZZER_PIN);
+      delay(duration * 1/3);
+    }
+  }
+
+  noTone(BUZZER_PIN);
+
+  isPlayingSong = false;
 }
